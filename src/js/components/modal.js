e783ae4871a840de8902c5468960526747ebfa2d
MoiseFlori <moiseflori@outlook.com>
2024-12-17 00:34:51 +0200

    am adaugat functionalitate pentru trailer si am stilizat modalul

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,5 +1,10 @@
-import { fetchPopularMovies, fetchMovieDetails } from '../api/moviesApi';
+import {
+  fetchPopularMovies,
+  fetchMovieDetails,
+  fetchTrailerUrl,
+} from '../api/moviesApi';
 import { generateMovieHTML } from '../components/movieList';
+import { initializeTrailerModal } from '../components/trailer';
 
 export async function initializeModal() {
   const movieList = document.querySelector('.movies');
@@ -14,21 +19,24 @@ export async function initializeModal() {
   const movieAbout = document.getElementById('movie-about');
   const trailerButton = document.getElementById('trailer-button');
 
+
+  const openTrailerModal = initializeTrailerModal();
+
   function renderMoviesModal(movies) {
-    // movieList.innerHTML = '';
-    // movies.forEach(movie => {
-    //   const movieCard = document.createElement('li');
-    //   movieCard.classList.add('movie_list_item');
-    //   movieCard.setAttribute('data-movie-id', movie.id);
-    //   movieCard.innerHTML = generateMovieHTML(movie);
+    movieList.innerHTML = '';
+    movies.forEach(movie => {
+      const movieCard = document.createElement('li');
+      movieCard.classList.add('movie_list_item');
+      movieCard.setAttribute('data-movie-id', movie.id);
+      movieCard.innerHTML = generateMovieHTML(movie);
 
-    //   movieCard.addEventListener('click', () => {
-    //     console.log('Clicked movie card:', movie.id);
-    //     handleMovieDetails(movie.id);
-    //   });
+      movieCard.addEventListener('click', () => {
+        console.log('Clicked movie card:', movie.id);
+        handleMovieDetails(movie.id);
+      });
 
-    //   movieList.appendChild(movieCard);
-    // });
+      movieList.appendChild(movieCard);
+    });
   }
 
   async function handleMovieDetails(movieId) {
@@ -44,10 +52,27 @@ export async function initializeModal() {
     posterSection.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${
       data.poster_path || ''
     }')`;
+
     movieTitle.textContent = data.title || 'No Title';
-    movieVotes.textContent = `${data.vote_average?.toFixed(1) || 'N/A'} / ${
-      data.vote_count || 0
-    }`;
+
+   
+    let ratingClass = 'movie__average--red';
+    if (data.vote_average >= 7) ratingClass = 'movie__average--green';
+    else if (data.vote_average >= 5) ratingClass = 'movie__average--orange';
+
+    const modalMovieVotes = document.querySelector('#modal-movie-votes');
+    if (modalMovieVotes) {
+      modalMovieVotes.innerHTML = `
+    <span class="vote-average ${ratingClass}">
+      ${data.vote_average?.toFixed(1) || 'N/A'}
+    </span>
+    <span class="vote-separator">/</span>
+    <span class="vote-count">
+      ${data.vote_count || 0}
+    </span>
+  `;
+    }
+
     moviePopularity.textContent = data.popularity?.toFixed(1) || 'N/A';
     originalTitle.textContent = data.original_title || 'N/A';
     movieGenre.textContent =
@@ -62,7 +87,7 @@ export async function initializeModal() {
     try {
       const trailerUrl = await fetchTrailerUrl(movieId);
       if (trailerUrl) {
-        window.open(trailerUrl, '_blank');
+        openTrailerModal(trailerUrl); 
       } else {
         alert('Trailer not available.');
       }
@@ -72,10 +97,12 @@ export async function initializeModal() {
   }
 
   function openModal() {
+    document.body.classList.add('modal-open');
     modalWrapper.classList.add('open');
   }
 
   function closeModal() {
+    document.body.classList.remove('modal-open');
     modalWrapper.classList.remove('open');
   }
 
