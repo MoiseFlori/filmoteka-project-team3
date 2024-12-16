608c8461f193ad771a2ce796e87236fab2504966
tanaseionclaudiu <tanaseionclaudiu@gmail.com>
2024-12-16 21:32:07 +0200

    update claudiu 3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './sass/index.scss';
+import './sass/_pagination.scss';
+
+import './js/components/searchBar';
+
 
 import { renderMovies } from './js/components/movieList.js';
 import { initializeModal } from './js/components/modal.js';
