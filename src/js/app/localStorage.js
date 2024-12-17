8cf7bc0938dda2f2a30d0306d44f0038b15b2a5e
MoiseFlori <moiseflori@outlook.com>
2024-12-17 12:33:58 +0200

    added functions to localStorage.js

--- src/js/app/localStorage.js
+++ src/js/app/localStorage.js
@@ -1,5 +1,24 @@
-// Gestionarea localStorage pentru salvarea și încărcarea filmelor:
-// addToWatchList(movie)
-// addToQueueList(movie)
-// getWatchList()
-// getQueueList()
+export function getFromStorage(key) {
+  return JSON.parse(localStorage.getItem(key)) || [];
+}
+
+export function isInStorage(key, id) {
+  const items = getFromStorage(key);
+  return items.some(item => item.id === id);
+}
+
+export function toggleStorage(key, movie) {
+  let items = getFromStorage(key);
+
+  if (isInStorage(key, movie.id)) {
+    items = items.filter(item => item.id !== movie.id);
+  } else {
+    items.push(movie);
+  }
+
+  localStorage.setItem(key, JSON.stringify(items));
+}
+
+export function saveToStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
