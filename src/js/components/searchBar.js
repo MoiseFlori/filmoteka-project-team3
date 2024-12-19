b605ffb4fbea7d1b825600f26168555068f2600c
MoiseFlori <moiseflori@outlook.com>
2024-12-19 19:28:10 +0200

    uptate searchBar pentru conflicte

--- src/js/components/searchBar.js
+++ src/js/components/searchBar.js
@@ -1,50 +1,28 @@
 // Logic for the search bar (including input events)
 
-
-console.log('*** Loaded searchbar.js');
-
-const searchText = document.querySelector('.search.form-input');
-const searchButton = document.querySelector('.search-button');  
-const pagination = document.querySelector('.pagination');  
-
-console.log(pagination);
-
 const searchText = document.querySelector('.search');
 const searchButton = document.querySelector('.search-button');
-
+let currentSearchQuery = ''; 
 
 import { renderSearchedMovies } from './movieList';
 
-
-searchText.addEventListener('click', function (e) {
-    showLoader();
-    e.preventDefault();
-    const page = 1;
-    renderSearchedMovies(searchText.value, page)
-});
-
-
-searchButton.addEventListener('click', function (e) {
-    showLoader();
-
-let currentSearchQuery = '';
-
-searchButton.addEventListener('click', async function (e) {
-  e.preventDefault();
+async function handleSearch(event) {
+  event.preventDefault();
   if (searchText.value.trim() === '') return;
-  currentSearchQuery = searchText.value.trim(); 
-  await renderSearchedMovies(currentSearchQuery, 1);
-  searchText.value = '';
-});
+
+  showLoader(); 
+  currentSearchQuery = searchText.value.trim();
+  await renderSearchedMovies(currentSearchQuery, 1); 
+  searchText.value = ''; 
+}
+
+
+searchButton.addEventListener('click', handleSearch);
+
 
 searchText.addEventListener('keydown', async function (e) {
   if (e.key === 'Enter') {
-
-    e.preventDefault();
-    if (searchText.value.trim() === '') return;
-    currentSearchQuery = searchText.value.trim();
-    await renderSearchedMovies(currentSearchQuery, 1);
-    searchText.value = '';
+    await handleSearch(e); 
   }
 });
 
